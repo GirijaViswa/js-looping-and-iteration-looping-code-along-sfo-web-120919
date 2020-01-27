41c5926f5693461f00ff9e2b22eca10fe771cb17
Girija Viswanathan <58586934+GirijaViswa@users.noreply.github.com>
2020-01-27 10:02:55 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function countDown(num)
+{   
+    while(num >= 0)
+    {
+        console.log(num);
+        num --;
+    }
+
+}
+
+function writeCards(str,event)
+{
+    let message = [];
+    for(let i=0;i<str.length;i++)
+    {
+        message.push(`Thank you, ${str[i]}, for the wonderful surprise gift!`);
+    }
+    return message;
+}
